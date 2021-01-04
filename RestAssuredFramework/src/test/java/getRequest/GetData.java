2c847a70d92b6678145e06575ac893b37a019159
syedhuq829 <75640346+syedhuq829@users.noreply.github.com>
2021-01-03 21:27:02 -0500

    added RestAssured maven dependencies

--- RestAssuredFramework/src/test/java/getRequest/GetData.java
+++ RestAssuredFramework/src/test/java/getRequest/GetData.java
@@ -1,11 +1,26 @@
 package getRequest;
 
+import org.openqa.selenium.remote.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
 public class GetData {
+	
+	@Test
 	
 	public void testResponsecode() {
 		
 	// test code
+		
+	io.restassured.response.Response resp = RestAssured.get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=d8217c6fec49b7cb225edd8a043b4a14");
+     
+	int code = resp.getStatusCode();
 	
+	System.out.println("Status code is " +code);
+	
+	Assert.assertEquals(code,200);
 	}
 
 }
---
+++ RestAssuredFramework/src/test/java/getRequest/runTest.java
@@ -0,0 +1,14 @@
+package getRequest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class runTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
