cffd9b701b39e77f5f367bfdef15c15fb8f6b2f6
syedhuq829 <75640346+syedhuq829@users.noreply.github.com>
2021-01-04 13:00:54 -0500

    added 2nd method

--- RestAssuredFramework/src/test/java/getRequest/GetData.java
+++ RestAssuredFramework/src/test/java/getRequest/GetData.java
@@ -23,4 +23,21 @@ public class GetData {
 	Assert.assertEquals(code,200);
 	}
 
+@Test
+	
+	public void testbody() {
+		
+	// test code
+		
+	io.restassured.response.Response resp = RestAssured.get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=d8217c6fec49b7cb225edd8a043b4a14");
+     
+	String data = resp.asString();
+	
+	System.out.println("Data is " +data);
+	
+	System.out.println("Response time "+resp.getTime());
+	//Assert.assertEquals(code,200);
+	}
+	
+	
 }
